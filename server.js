75dfdce3b4ebc461535fde9a9cee2080b30b80bf
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-10-16 00:25:25 +0100

    added products

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
-// const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+const DB_ONLINE =
+  "mongodb+srv://giddy:BqGOcPI8FD0DL2K7@cluster0.7rupp.mongodb.net/EcommerceDB?retryWrites=true&w=majority";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("we are ready");
-});
+mongoose
+  .connect(DB_ONLINE, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(() => {
+    console.log("an error occured while connecting to the server");
+  });
+
+app.use(cors());
+
+app.use("/api", require("./router"));
 
 app.listen(port, () => {
   console.log(`listening on Port ${port}`);
